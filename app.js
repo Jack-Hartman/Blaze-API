97914cbfbd24e9eff7942f976039fe5a06ba95d0
Jack Hartman <jack27pg@gmail.com>
2021-08-11 00:19:09 +0100

    Error Code for when there are no servers

--- app.js
+++ app.js
@@ -8,18 +8,19 @@ app.get(`/${base}/`, (req, res) => {
     return res.json({code: 200, message: "operational"});
 });     
 
-app.get(`/${base}/ServerList`, (req, res) => { // Returns the list of servers for a given appID from the steamAPI
+app.get(`/${base}/ServerList`, (req, res) => { // Returns the list of servers for a given appID from the SteamAPI
     const query = url.parse(req.url, true).query;
     if (query !== null && query.appID !== undefined && query.appID !== "") {
         axios.get(`https://api.steampowered.com/IGameServersService/GetServerList/v1/?filter=\\appid\\${query.appID}&key=${process.env.API_KEY}`)
             .then(function (response) {
                 if (response.status === 200) {
-                    /* Server Map
+                    /* Server Map/Filtering
                     let data = response.data.response.servers.map((server) => {   
                         let ipAddr = server.addr.split(":")[0];
                         return { ServerName: server.name, Map: server.map, CurrentPlayers: server.players, MaxPlayers: server.max_players, IP: ipAddr, Port: server.gameport, SteamID: server.steamid, VAC: server.secure }
                     });*/
-                    return res.json({ code: 200, data: response.data.response.servers });
+                    if (response.data.response.servers.length > 0) return res.json({ code: 200, data: response.data.response.servers });
+                    else return res.json({ code: 404, message: "Servers not found" });
                 }
                 else {
                     return res.json({ code: 500, message: "Internal server error" });
